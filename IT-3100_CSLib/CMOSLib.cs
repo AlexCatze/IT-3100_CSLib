f0cf8ed4cd01f410775cd24ef506ca73eaef7541
AlexCatze <alexcatze@gmail.com>
2023-05-14 16:31:46 +0300

    Init commit

---
+++ IT-3100_CSLib/CMOSLib.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace IT_3100_CsLib
+{
+    public class CMOSLib
+    {
+        const string CMOS_LIB = "IMGdecodece.dll";
+
+        static private int stream_x = 0, stream_y = 0;
+
+        public enum ImageFormat
+        {
+            IMAGE_2MONO,
+            UNDEFINED,
+            IMAGE_256MONO
+        }
+
+        public enum CMOSStatus : int
+        {///NOT CONFIRMED
+        }
+        /*
+            IMG_SUCCESS : Normal termination
+            IMG_ERR_MEMORY : Memory error has occurred.
+            IMG_ERR_UNSUPPORTED : The specified operation cannot be executed, since the imager is not integrated in the terminal.
+         * 
+            IMG_ERR_NOTINITIALIZED : IMGInit function is not executed.
+            IMG_ERR_NOTCONNECTED : IMGConnect function is not executed.
+            IMG_ERR_ENGINEBUSY : Error has occurred due to the busy state of the imager device.
+            IMG_ERR_PARAMETER : An error has occurred due to an illegal parameter.
+            IMG_ERR_NOTRIGGER : This function is terminated by the user function, or the decoding is terminated by IMGStopDecode function.
+            IMG_ERR_NODECODE : Terminated without decoding.
+            IMG_ERR_NOIMAGE : No valid image data can be returned.
+         * 
+            IMG_ERR_DRIVER : An error has occurred in the imager driver.
+         * 
+            IMG_ERR_NORESPONSE : No response from the device driver.
+            IMG_ERR_BADREGION : The specified coordinate is out of the allowable range.
+         */
+
+        public enum decodeMode : int
+        {///NOT CONFIRMED
+            IMG_DECODEMODE_NORMAL, 
+            IMG_DECODEMODE_MULTISTEP, 
+            IMG_DECODEMODE_PACKAGE 
+        }
+
+        public enum scanMode : int
+        {///NOT CONFIRMED
+            IMG_SCAN_MODE_OUTDOOR,
+            IMG_SCAN_MODE_WINDOWSIDE,
+            IMG_SCAN_MODE_INDOOR,
+            IMG_SCAN_MODE_WAREHOUSE
+        }
+
+        public enum ledMode : int
+        {///NOT CONFIRMED
+            IMG_LEDON,
+            IMG_LEDOFF,
+            IMG_LEDEROF
+        }
+
+        public enum buzzerMode : int
+        {///NOT CONFIRMED
+            IMG_BUZON,
+            IMG_BUZOFF
+        }
+
+        public enum deliberationMode : int
+        {///NOT CONFIRMED
+            IMG_DECODE_VERYQUICK,
+            IMG_DECODE_QUICK,
+            IMG_DECODE_NORMAL,
+            IMG_DECODE_DELIBERATE,
+            IMG_DECODE_VERYDELIBERATE
+        }
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGInit();
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGDeinit();
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGConnect();
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGDisconnect();
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetDecodeMode(ref Int32 pMode, ref Int32 pNum, string ptcSeparator);
+        //pMode - decodeMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetDecodeMode(Int32 dwMode, Int32 dwNum, string tcSeparator);
+        //dwMode - decodeMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGWaitForDecode(Int32 dwTime, string pMessage, string pCodeID, string pAimID, string pSymModifier, ref Int32 pLength, IntPtr fpCallBack);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGWaitForDecodeRaw(Int32 dwTime, ref byte pMessage, string pCodeID, string pAimID, string pSymModifier, ref Int32 pLength, IntPtr fpCallBack);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGStopDecode();
+
+        [DllImport(CMOS_LIB)]
+        private unsafe static extern Int32 IMGGetImage(byte* pImageBuffer, ref Int32 pSize, Int32 nLeft, Int32 nTop, Int32 nRight, Int32 nBottom, Int32 dwSkip, Int32 dwFormat, Int32 dwWhiteValue);
+
+        public static Int32 IMGGetImage(out byte[] pImageBuffer, int x, int y, ImageFormat dwFormat, Int32 dwWhiteValue)
+        {
+            if (x > 640 || y > 480 || x<1 || y<1) throw new FormatException("Wrong picture size");
+            int pSize = 0;
+            pImageBuffer = new byte[x * y];
+            unsafe
+            {
+                fixed (byte* iptr = pImageBuffer)
+                {
+                    return IMGGetImage(iptr, ref pSize, 0, 0, x, y, 1, (int)dwFormat, dwWhiteValue);//FIXME support lower resolution
+                }
+            }
+        }
+        
+        [DllImport(CMOS_LIB)]
+        private static extern Int32 IMGStartStream(Int32 nLeft, Int32 nTop, Int32 nRight, Int32 nBottom, Int32 dwSkip, Int32 dwFormat);
+
+        public static Int32 IMGStartStream(int x, int y, ImageFormat dwFormat)
+        {
+            //if (x > 640 || y > 480 || x < 1 || y < 1) throw new FormatException("Wrong picture size");
+            stream_x = x; stream_y = y;
+            return IMGStartStream(0, 0, x, y, 1, (int)dwFormat);
+        }
+
+        [DllImport(CMOS_LIB)]
+        private unsafe static extern Int32 IMGGetStreamData(byte* pImageBuffer, ref Int32 pSize);
+
+        public static Int32 IMGGetStreamData(out byte[] pImageBuffer)
+        {
+            int pSize = 0;
+            pImageBuffer = new byte[stream_x*stream_y];
+            unsafe
+            {
+                fixed (byte* iptr = pImageBuffer)
+                {
+                    return IMGGetStreamData(iptr, ref pSize);
+                }
+            }
+        }
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGStopStream();
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGCaptureSign(ref byte pImageBuffer, Int32 dwAspectRatio, Int32 nOffsetX, Int32 nOffsetY, UInt32 dwWidth, UInt32 dwHeight, Int32 nResolution, Int32 dwFormat);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGAimerOn(bool bEnable);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGIlluminationOn(bool bEnable);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetAimer(ref Int32 pAim);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetAimer(Int32 dwAim);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetIllumination(ref Int32 pIll);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetIllumination(Int32 dwIll);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetScanMode(ref Int32 pScanMode);
+        //pScanMode - scanMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetScanMode(Int32 dwScanMode);
+        //pScanMode - scanMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetImagerAPO(ref Int32 pTimeOut);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetImagerAPO(Int32 dwTimeOut);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetPrintWeight(ref Int32 pPrintWeight);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetPrintWeight(Int32 dwPrintWeight);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetLED(ref Int32 pLED);
+        //pLED - ledMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetLED(Int32 dwLED);
+        //dwLED - ledMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetBuzzer(ref Int32 pBuzzer);
+        //pBuzzer - buzzerMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetBuzzer(Int32 dwBuzzer);
+        //dwBuzzer - buzzerMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGGetDeliberation(ref Int32 pDeliberateTime);
+        //pDeliberateTime - deliberationMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSetDeliberation(Int32 dwDeliberateTime);
+        //dwDeliberateTime - deliberationMode
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGLoadConfigFile(string pFileName);
+
+        [DllImport(CMOS_LIB)]
+        public static extern Int32 IMGSaveConfigFile(string pFileName);
+    }
+}
---
+++ IT-3100_CSLib/PrinterLib.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace IT_3100_CsLib
+{
+    public class PrinterLib
+    {
+        const string PRINTER_LIB = "prnlib.dll";
+        /*
+         1    0 00001A89 PRNBMPOut//
+         2    1 000019DD PRNBarcodeOut//
+         3    2 00001B05 PRNCheckMarker//
+         4    3 00001689 PRNClose//
+         5    4 000020BD PRNGetDrverVersion
+         6    5 00001BED PRNGetLastError//
+         7    6 00002049 PRNGetLogData
+         8    7 00001CD9 PRNGetPaperWidth//
+         9    8 00001FB9 PRNGetPrintData
+        10    9 00001F41 PRNGetPrintDataHeigth
+        11    A 00001DF9 PRNGetPrinterProperty//
+        12    B 00002155 PRNGetPrinterStatus
+        13    C 00001B79 PRNGetStatus//
+        14    D 00001941 PRNImageOut//
+        15    E 00001751 PRNInitializePrinter//
+        16    F 000014D1 PRNOpen//
+        17   10 000017C5 PRNPrintScreen//
+        18   11 00001839 PRNPrintWindow//
+        19   12 00001EB1 PRNRegisterParameterTabel
+        20   13 00001C61 PRNSetPaperWidth//
+        21   14 00001D51 PRNSetPrinterProperty//
+        22   15 000018B1 PRNTextOut//
+       */
+
+        public enum PRNStatus : uint {
+            PRN_NORMAL,
+        	PRN_NOTOPEN,
+            PRN_DRIVER_NOTEXIST,
+        	PRN_ALREADY_OPEN,
+        	PRN_NOTFOUND,
+        	PRN_NOTCHANGE,
+        	PRN_FILE_NOTEXIST,
+        	PRN_FILEOPEN_ERROR,
+        	PRN_FILEFORMAT_ERROR,
+        	PRN_PARAMETER_ERROR,
+        	PRN_HARDWARE_ERROR,
+        	PRN_PLATEN_OPEN,
+        	PRN_PAPER_END,
+        	PRN_VDETP_OCCURRED,
+        	PRN_SUSPEND_OCCURRED,
+        	PRN_HEADTEMP_ERROR,
+	        PRN_AUTOLOADING,
+            PRN_FEEDKEY_ERROR
+        }
+
+        /*
+        0	PRN_NORMAL : Normal end.
+        1	PRN_NOTOPEN : Printer has not been set operable.
+        //  PRN_DRIVER_NOTEXIST : Printer driver does not reside causing the printer not to be operable.
+        3	PRN_ALREADY_OPEN : Already opened or occupied by other application software.
+        	PRN_NOTFOUND : Marker cannot be found.
+        	PRN_NOTCHANGE : Paper width cannot be changed.
+        6	PRN_FILE_NOTEXIST : Specified file cannot be found.
+        7	PRN_FILEOPEN_ERROR : Specified file cannot be opened.
+        	PRN_FILEFORMAT_ERROR : Specified file is with illegal format.
+        9	PRN_PARAMETER_ERROR : Parameter error.
+        	PRN_HARDWARE_ERROR : Hardware of the printer is malfunction.
+        11	PRN_PLATEN_OPEN : Platen of the printer is kept open.
+        12	PRN_PAPER_END : Paper is not loaded.
+        	PRN_VDETP_OCCURRED : VDETP has occurred.
+        	PRN_SUSPEND_OCCURRED : Suspend has occurred.
+        	PRN_HEADTEMP_ERROR : Error has occurred due to abonormal temperature in the printer head.
+	        PRN_AUTOLOADING : Paper is being loaded automatically.
+        17  PRN_FEEDKEY_ERROR : Paper feeding with the feed key is in operation causing the printer not to be operable.
+         */
+
+        public enum paperWidth : uint
+        {
+            WIDE80mm,
+            STRAIT58mm
+        }
+
+        public enum codeType : uint
+        {
+            JAN,
+            NW7,
+            Code39,
+            ITF,
+            UPCE,
+            Code128
+        }
+
+        public enum fontType : uint
+        {
+            NONE,
+            ANK8x16,
+            ANK6x7,
+            OCRBI
+        }
+
+        public enum direction : uint
+        {
+            VERTICAL,
+            HORIZONTAL
+        }
+
+        public enum paperType : uint
+        {
+            F200U9W5,
+            HS360,
+            AFP235,
+            HG56S,
+            TLC00,
+            CUSTOM
+        }
+
+        public enum printMode : uint
+        {
+            SPEED,
+            QUALITY,
+            GRAPHIC
+        }
+
+        public struct printerSettings
+        {
+            public paperType dwPaperType;
+            public uint dwDepth;
+            public printMode dwSpeed;
+            public bool dwAutoloading;
+            public uint dwAutolodingLength;
+            public bool dwPreheat;
+            public bool dwPrintContinuation;
+        }
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNOpen();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNClose();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNPrintScreen();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNInitializePrinter();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNPrintWindow(IntPtr hWindow);
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNTextOut(uint dwLength, string szTextData);
+
+        public static PRNStatus PRNTextOut(string szTextData)
+        { return PRNTextOut(0, szTextData); }
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNImageOut(uint dwWidth, uint dwHeight, uint dwFeedLength, byte[] pbyImageData);
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNBarcodeOut(codeType dwCode, uint dwHeight, bool dwCheckDigit, fontType dwFont, uint dwLeftMargin, direction dwDirection, uint dwLength, string szBarcodeData);
+        
+        public static PRNStatus PRNBarcodeOut(codeType dwCode, uint dwHeight, bool dwCheckDigit, fontType dwFont, uint dwLeftMargin, direction dwDirection, string szBarcodeData)
+        { return PRNBarcodeOut(dwCode, dwHeight, dwCheckDigit, dwFont, dwLeftMargin, dwDirection, (uint)(szBarcodeData.Length + (dwCheckDigit ? 1 : 0)), szBarcodeData); }
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNBMPOut(string szFilename);
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNCheckMarker();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNGetStatus();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNGetLastError();
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNSetPaperWidth(paperWidth dwWidth);
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNGetPaperWidth(ref paperWidth dwWidth);
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNSetPrinterProperty(paperType dwPaperType, uint dwDepth, printMode dwSpeed, bool dwAutoloading, uint dwAutolodingLength, bool dwPreheat, bool dwPrintContinuation);
+
+        public static PRNStatus PRNSetPrinterProperty(printerSettings prnSet)
+        {
+            return PRNSetPrinterProperty(prnSet.dwPaperType, prnSet.dwDepth, prnSet.dwSpeed, prnSet.dwAutoloading, prnSet.dwAutolodingLength, prnSet.dwPreheat, prnSet.dwPrintContinuation);
+        }
+
+        [DllImport(PRINTER_LIB)]
+        public static extern PRNStatus PRNGetPrinterProperty(ref paperType dwPaperType, ref uint dwDepth, ref printMode dwSpeed, ref bool dwAutoloading, ref uint dwAutolodingLength, ref bool dwPreheat, ref bool dwPrintContinuation);
+
+        public static PRNStatus PRNGetPrinterProperty(ref printerSettings prnSet)
+        {
+            return PRNGetPrinterProperty(ref prnSet.dwPaperType, ref prnSet.dwDepth, ref prnSet.dwSpeed, ref prnSet.dwAutoloading, ref prnSet.dwAutolodingLength, ref prnSet.dwPreheat, ref prnSet.dwPrintContinuation);
+        }
+    }
+}
